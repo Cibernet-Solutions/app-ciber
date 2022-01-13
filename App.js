0cf75105ee7defd5b760dc7e110216a3c9f76b75
Tales <tales.monteiro@hotmail.com>
2022-01-13 16:25:25 -0300

    Corrigindo as permissoes

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import * as React from 'react';
+/* import * as React from 'react'; */
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, StyleSheet, BackHandler } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { Camera } from 'expo-camera'
+import { Audio } from 'expo-av'
+import { ActivityIndicator } from 'react-native';
+import { url } from './constants';
 
 export default function App() {
+
+  const webViewRef = useRef(null)
+  const Spinner = () => (
+    <View style={styles.activityContainer}>
+      <ActivityIndicator size="large" color="#f29900" />
+    </View>
+  );
+
+  useEffect(() => {
+    (async () => {
+
+      await Audio.requestPermissionsAsync();
+      await Camera.requestCameraPermissionsAsync();
+
+    })();
+  }, []);
+  useEffect(() => {
+    const backAction = () => {
+      webViewRef.current.goBack();
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => backHandler.remove();
+  }, []);
+
+
   return (
-    <WebView 
-      style={styles.container}
-      source={{ uri: 'https://homologacao.cibernetsolutions.com.br/geral/app/mobile/login.cshtml' }}
-    />
+    <View style={styles.container}>
+      <WebView
+        style={styles.view}
+        javaScriptEnabled
+        pullToRefreshEnabled
+        source={{
+          uri: url,
+        }}
+        startInLoadingState
+        allowsInlineMediaPlayback
+        allowsFullscreenVideo
+        domStorageEnabled
+        useWebKit
+        originWhitelist={["*"]}
+        mediaPlaybackRequiresUserAction={false}
+
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
+    backgroundColor: '#006837',
+    paddingTop: 40
   },
+  activityContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: '#fff',
+    height: '100%',
+    width: '100%'
+  },
+  view: {
+    borderColor: 'red',
+
+  }
 });
---
+++ constants.ts
@@ -0,0 +1,2 @@
+export const url = "https://homologacao.cibernetsolutions.com.br/geral/app/mobile/login.cshtml";
+/*"http://medsysnet.biosaude.med.br/medsysnetapp/mobile/login";*/
